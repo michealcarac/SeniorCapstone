d438f93af7c0d10ef37eec3983a7e87f70ad301f
Aaron Jones <89935624+aarjones@users.noreply.github.com>
2022-10-25 20:15:09 -0400

    Outlined keylogger class

---
+++ Software/include/keylogger.hpp
@@ -0,0 +1,48 @@
+/* Project: Clarkson University Capstone 
+   Writer(s): Aaron R. Jones
+   Last Edited: 10/25/2022 
+   Purpose: This file describes the Keylogger class.
+*/
+
+#ifndef KEYLOGGER_H
+#define KEYLOGGER_H
+
+/* DEFINES */
+
+/* INCLUDES */
+#include <vector>
+#include "modes.hpp"
+#include "profile.hpp"
+#include "keypresses.hpp"
+
+/* NAMESPACES */
+using std::vector;
+
+class Keylogger {
+    public:
+        /* CONSTRUCTORS */
+        Keylogger(); // constructor
+
+        /* FUNCTIONS */
+        void runAuth();  // runs the authentication algorithm
+        void runTrain(); // runs the training algorithm
+        void runDisable(); // runs the "disabled" mode
+        void nextMode(); // moves to the next mode
+        void nextProfile(); // moves to the next profile, including entries for "New" and "Delete"
+
+        /* ACCESSORS */
+        Modes getCurrentMode(); // returns the current mode
+        Profile getCurrentProfile(); // returns the current Profile object 
+        
+        /* MUTATORS */
+        void setCurrentMode(Modes newMode); // sets the system to the requested mode
+        void setCurrentProfile(int newProfile); // moves to the specified entry in the profiles vector
+
+    private:
+        Modes currentMode;
+        int currentProfile;
+        vector<Profile> profiles;
+        Keypresses *presses;
+};
+
+#endif
